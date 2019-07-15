06cde70d76e70f5795df03fcbbb38b793355a0fb
tmartinez114 <tmartinez114@gmail.com>
2019-07-15 17:53:22 -0400

    commented out some of the fields to test in Insomnia

--- mern-radio-inv/backend/routes/radios.js
+++ mern-radio-inv/backend/routes/radios.js
@@ -9,17 +9,17 @@ router.route("/").get((req, res) => {
 
 router.route("/add").post((req, res) => {
   const model = req.body.model;
-  const serial = req.body.serial;
-  const mdcid = req.body.mdcid;
-  const assigned = req.body.assigned;
-  const date = Date.parse(req.body.date);
+  //   const serial = req.body.serial;
+  //   const mdcid = req.body.mdcid;
+  //   const assigned = req.body.assigned;
+  //   const date = Date.parse(req.body.date);
 
   const newRadio = new Radio({
-    model,
-    serial,
-    mdcid,
-    assigned,
-    date
+    model
+    // serial,
+    // mdcid,
+    // assigned,
+    // date
   });
 
   newRadio
