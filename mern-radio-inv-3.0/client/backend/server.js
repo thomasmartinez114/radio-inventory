8f0f8aca1225414bc19636a4b8c5c21af7d58895
tmartinez114 <tmartinez114@gmail.com>
2019-10-24 16:37:00 -0400

    connected mongodb

--- mern-radio-inv-3.0/client/backend/server.js
+++ mern-radio-inv-3.0/client/backend/server.js
@@ -17,7 +17,7 @@ const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
 connection.once('open', () => {
-  console.log('MongoDB database connection established successfully');
+  console.log('MongoDB connection established successfully');
 });
 
 app.listen(port, () => {
