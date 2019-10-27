4cf3e2ea4ab7e5d17a13970f467d2c816615bb5a
tmartinez114 <tmartinez114@gmail.com>
2019-10-27 18:11:48 -0400

    imported components

--- main-version/radio-tracker/src/App.js
+++ main-version/radio-tracker/src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/boostrap.min.css';
 
+// Import Routes
+import Navbar from './components/navbar.component';
+import RadioList from './components/radio-list.component';
+import EditRadio from './components/edit-radio.component';
+import CreateRadio from './components/create-radio.component';
+
 function App() {
   return (
     <Router>
