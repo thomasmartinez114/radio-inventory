190b10a34662cad1e01870a5f83b7fc127833b7c
tmartinez114 <tmartinez114@gmail.com>
2019-10-27 17:22:36 -0400

    added routes to get/delete/update radios by ID

--- main-version/radio-tracker/backend/routes/radios.js
+++ main-version/radio-tracker/backend/routes/radios.js
@@ -28,4 +28,42 @@ router.route('/add').post((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+// @route   GET api/radios/:id
+// @desc    Get radio by ID
+// @access  Public
+router.route('/:id').get((req, res) => {
+  Radio.findById(req.params.id)
+    .then(radios => res.json(radios))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// @route   DELETE api/radios/:id
+// @desc    DELETE radio by ID
+// @access  Public
+router.route('/:id').delete((req, res) => {
+  Radio.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Radio deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// @route   POST api/radios/:id
+// @desc    Update radio by ID
+// @access  Public
+router.route('/update/:id').post((req, res) => {
+  Radio.findById(req.params.id)
+    .then(radio => {
+      radio.model = req.body.model;
+      radio.serial = req.body.serial;
+      radio.mdcid = req.body.mdcid;
+      radio.assigned = req.body.assigned;
+      radio.dateIssued = req.body.dateIssued;
+
+      radio
+        .save()
+        .then(() => res.json('Radio update!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
 module.exports = router;
