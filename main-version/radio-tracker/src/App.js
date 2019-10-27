84c2eccffcd87a4c5b953a2ecedb79ed314cf942
tmartinez114 <tmartinez114@gmail.com>
2019-10-27 18:47:32 -0400

    resolved bug from importing bootstrap typo

--- main-version/radio-tracker/src/App.js
+++ main-version/radio-tracker/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import 'bootstrap/dist/css/boostrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Import Routes
 import Navbar from './components/navbar.component';
