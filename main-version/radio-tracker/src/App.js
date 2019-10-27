29948cf2ab025386de1905d83662d1ce6d7e7cbd
tmartinez114 <tmartinez114@gmail.com>
2019-10-27 18:08:25 -0400

    added components

--- main-version/radio-tracker/src/App.js
+++ main-version/radio-tracker/src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/boostrap.min.css';
 
 function App() {
-  return <div className='container'>Hello World!</div>;
+  return (
+    <Router>
+      <Navbar />
+      <br />
+      <Route path='/' exact component={RadioList} />
+      <Route path='/edit/:id' exact component={EditRadio} />
+      <Route path='/create' exact component={CreateRadio} />
+    </Router>
+  );
 }
-
 export default App;
