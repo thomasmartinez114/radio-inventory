7eb3ffd82f03827ba582bc6c0ecb6c51bc9aca34
tmartinez114 <tmartinez114@gmail.com>
2019-10-27 14:23:31 -0400

    added .env with db info

---
+++ main-version/radio-tracker/backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+// Server
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect MongoDB
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MongoDB database connectino established successfully');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
