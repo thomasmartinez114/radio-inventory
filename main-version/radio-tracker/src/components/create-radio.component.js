119f7f8619ffb9e30dc5646dc03d4459470f6305
tmartinez114 <tmartinez114@gmail.com>
2019-10-28 20:21:35 -0400

    put component back to default

--- main-version/radio-tracker/src/components/create-radio.component.js
+++ main-version/radio-tracker/src/components/create-radio.component.js
@@ -4,54 +4,8 @@ export default class CreateUser extends Component {
   render() {
     return (
       <div>
-        <p>You are on the Create User component!</p>
+        <p>You are on the Create Radio component!</p>
       </div>
     );
   }
 }
-
-// import React, { Component } from 'react';
-
-// export default class CreateRadio extends Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.onChangeModel = this.onChangeModel.bind(this);
-//     this.onChangeSerial = this.onChangeSerial.bind(this);
-//     this.onChangeMdcid = this.onChangeMdcid.bind(this);
-//     this.onChangeAssigned = this.onChangeAssigned.bind(this);
-
-//     this.state = {
-//       model: '',
-//       serial: '',
-//       mdcid: '',
-//       assigned: ''
-//     };
-//   }
-
-//     onChangeModel(e) {
-//       this.setState({
-//         model: e.target.value
-//       });
-//     }
-
-//     onChangeSerial(e) {
-//       this.setState({
-//         serial: e.target.value
-//       });
-//     }
-
-//     onChangeMdcid(e) {
-//       this.setState({
-//         mdcid: e.target.value
-//       });
-//     }
-
-//     onChangeAssigned(e) {
-//       this.setState({
-//         assigned: e.target.value
-//       });
-//     }
-
-// End of component
-// }
